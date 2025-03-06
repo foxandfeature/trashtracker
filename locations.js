4e5e1661f3e4ad9f8db6708af95ac0e25d27d67e
foxandfeature <foxandfeature@gmail.com>
2025-03-06 20:36:51 +0100

    Initial commit

---
+++ locations.js
@@ -0,0 +1,4 @@
+var locations = [{
+    "type": "Point",
+    "coordinates": [9.783225081686828, 52.28165009176988]
+}];
---
+++ map.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([52.265160276113164, 9.761516161742293], 15);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+map.locate({ setView: true });
+
+function onLocationFound(e) {
+    L.marker(e.latlng).addTo(map)
+}
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+map.on('locationerror', onLocationError);
+
+var icon = L.icon({
+    iconUrl: 'trash-can.png',
+    iconSize:     [30, 30],
+    iconAnchor:   [15, 15],
+});
+
+L.geoJSON(locations, {
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon: icon})
+    }
+}).addTo(map);

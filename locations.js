8ebba282ae250f55e73da27590cb0c545bcba454
foxandfeature <foxandfeature@gmail.com>
2025-03-09 20:05:21 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,15 +1,16 @@
 async function loadLocations() {
-    let locations = []
+   
     let response = await fetch("locations.csv");
     if (response.status != 200) {
         throw new Error("Server Error");
     }
     let text_data = await response.text();
-    console.log(text_data)
+    let locations = []
     text_data.split("\n").forEach(function (value) {
         let coordinates = value.split(";")
         locations.push(L.latLng(Number(coordinates[0]), Number(coordinates[1])));
     })
+    console.log(locations)
     let markers = []
     locations.forEach(function (value) {
         markers.push(L.marker(value, { icon: trashcanIcon }));

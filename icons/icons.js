07778c64e52b4fc2d490b4be65e03d879f87d9c7
foxandfeature <foxandfeature@gmail.com>
2025-03-23 20:34:48 +0100

    -

--- icons/icons.js
+++ icons/icons.js
@@ -1,15 +1,15 @@
-const locationMarkerDiv = document.createElement("div");
-const locationMarkerImg = document.createElement("img");
-locationMarkerImg.id = "location-marker-icon"
-locationMarkerImg.src = "icons/location_marker.svg"
-const directionMarkerImg = document.createElement("img");
-directionMarkerImg.id = "direction-marker-icon"
-directionMarkerImg.src = "icons/direction_marker.svg"
+const locationMarkerDiv = document.createElement('div');
+const locationMarkerImg = document.createElement('img');
+locationMarkerImg.id = 'location-marker-icon'
+locationMarkerImg.src = 'icons/location_marker.svg'
+const directionMarkerImg = document.createElement('img');
+directionMarkerImg.id = 'direction-marker-icon'
+directionMarkerImg.src = 'icons/direction_marker.svg'
 locationMarkerDiv.appendChild(locationMarkerImg)
 locationMarkerDiv.appendChild(directionMarkerImg);
 
 const locationIcon = L.divIcon({
-    className: "location-marker-div",
+    className: 'location-marker-div',
     html: locationMarkerDiv
 });
 const trashcanIcon = L.icon({
